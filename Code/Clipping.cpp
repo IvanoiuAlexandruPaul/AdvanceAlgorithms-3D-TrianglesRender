ed2c942d36232b6ca1812db8b4e76b49f8cc8e87
IvanoiuAlexandruPaul <48321600+IvanoiuAlexandruPaul@users.noreply.github.com>
2020-11-09 11:49:17 +0100

    Add files via upload

--- Code/Clipping.cpp
+++ Code/Clipping.cpp
@@ -1,69 +1,151 @@
 #include "Clipping.h"
 
-std::vector<Triangle> Clipping::clip(std::vector<Triangle>& triangles)
-{
-	std::vector<Vertex> auxiliartriangles;
-	std::vector<Triangle> finalListOfTriangles;
-	std::vector<Vertex> all_vertex = {};
-	for (Triangle t : triangles)
-	{
-		for (int i = 0; i <= 3; ++i){
-			std::vector<Vertex> vertex = t.get_vertex();
-			aux(vertex , i, auxiliartriangles, finalListOfTriangles);
-		}
-	}
-
-	return finalListOfTriangles;
-};
-
-void Clipping::buildTriangles(std::vector<Vertex>& listOfTheVertices, std::vector<Triangle>& finalListOfTriangles)
-{
-
-	Vertex tempVertex = listOfTheVertices[0];
-	listOfTheVertices.erase(listOfTheVertices.begin() + 0);
-	for (int i = 0; i <= listOfTheVertices.size(); ++i)
-	{
-		if (listOfTheVertices.size() >= 2)
-		{
-			Triangle TempTriangle = Triangle(tempVertex, listOfTheVertices[i], listOfTheVertices[i + 1]);
-			listOfTheVertices.erase(listOfTheVertices.begin() + 0);
-			finalListOfTriangles.push_back(TempTriangle);
-		}
-	}
+double linearInterpolation(double a, double b) {
+    return (1 - a) / (1 - a - (1 - b));
 }
 
-void Clipping::aux(std::vector<Vertex>& vertices, int component_Index, std::vector<Vertex>& result_vertices, std::vector<Triangle>& finalListOfTriangles)
-{
+Vertex interpolate(Vertex a, Vertex b) {
+    double lx = linearInterpolation(a.get_x(), b.get_x());
+    double ly = linearInterpolation(a.get_y(), b.get_y());
+    double lz = linearInterpolation(a.get_z(), b.get_z());
 
-	Vertex previousVertex = vertices[(vertices.size() - 1)]; //l'ultimo vertex nella lista
-	double previousComponent = previousVertex[component_Index];
-	bool previousInside = previousComponent <= previousVertex.get_w(); // se il nostro componente è dentro la zona di clipping
+    return Vertex(lx, ly, lz);
+}
 
-	for (auto &it : vertices)
-	{
-		Vertex currentVertex = it;
-		double currentComponent = currentVertex[component_Index];
-		bool currentInside = currentComponent <= currentVertex.get_w();
+std::vector<Triangle> Clipping::clip(std::vector<Triangle> &triangles) {
+    std::vector<Vertex> auxiliartriangles;
+    std::vector<Triangle> finalListOfTriangles;
+    std::vector<Vertex> all_vertex = {};
+    for (Triangle t : triangles) {
+        for (int i = 0; i <= 3; ++i) {
+            std::vector<Vertex> vertex = t.get_vertex();
+            aux(vertex, i, auxiliartriangles, finalListOfTriangles);
+        }
+    }
 
-		if (currentInside ^ previousInside) // se il nostro current è dentro e il previous non lo è   o viceversa
-		{
-			// L = (1-B)/(1-B)-(1-C)
-			double lerpAmt =  (currentVertex.get_w() - currentComponent) /
-							 ((currentVertex.get_w() - currentComponent) -
-							  (previousVertex.get_w() - previousComponent));
-
-			//This is the vertex that you will find inside the screen not ouside
-			result_vertices.push_back(lerp(currentVertex, previousVertex, lerpAmt));
-		}
-
-		if (currentInside)
-		{
-			result_vertices.push_back(currentVertex);
-		}
-
-		previousVertex = currentVertex;
-		previousComponent = currentComponent;
-		previousInside = currentInside;
-	}
-	buildTriangles(result_vertices, finalListOfTriangles);
+    return finalListOfTriangles;
 };
+
+/*
+
+ for triangolo in triangoli:
+    if c'è un vertice fuori da -1, 1:
+        - Rompi il triangolo in vertici tutti dentro -1, 1
+        - Raggruppa i nuovi vertici in triangoli.
+
+ */
+
+void Clipping::buildTriangles(std::vector<Vertex> &listOfTheVertices, std::vector<Triangle> &finalListOfTriangles) {
+    Vertex tempVertex = listOfTheVertices[0];
+    listOfTheVertices.erase(listOfTheVertices.begin() + 0);
+    for (int i = 0; i <= listOfTheVertices.size() - 1; ++i) {
+        Triangle TempTriangle = Triangle(tempVertex, listOfTheVertices[i], listOfTheVertices[i + 1]);
+        listOfTheVertices.erase(listOfTheVertices.begin() + 0);
+        finalListOfTriangles.push_back(TempTriangle);
+
+    }
+
+}
+
+void Clipping::aux(std::vector<Vertex> &vertices, int component_Index, std::vector<Vertex> &result_vertices,
+                   std::vector<Triangle> &finalListOfTriangles) {
+
+    Vertex previousVertex = vertices[(vertices.size() - 1)]; //l'ultimo vertex nella lista
+    double previousComponent = previousVertex[component_Index];
+    bool previousInside =
+            previousComponent <= previousVertex.get_w(); // se il nostro componente è dentro la zona di clipping
+
+    for (auto &it : vertices) {
+        Vertex currentVertex = it;
+        double currentComponent = currentVertex[component_Index];
+        bool currentInside = currentComponent <= currentVertex.get_w();
+
+        if (previousInside && !currentInside) {
+            Vertex v = interpolate(previousVertex, currentVertex);
+            //This is the vertex that you will find inside the screen not ouside
+            result_vertices.push_back(v);
+        } else if (currentInside && !previousInside) {
+            Vertex v = interpolate(currentVertex, previousVertex);
+            result_vertices.push_back(v);
+        }
+        /*
+        if (currentInside ^ previousInside) // se il nostro current è dentro e il previous non lo è o viceversa
+        {
+            // L = (1-B)/(1-B)-(1-C)
+            double lerpAmt = (currentVertex.get_w() - currentComponent) /
+                             ((currentVertex.get_w() - currentComponent) -
+                              (previousVertex.get_w() - previousComponent));
+            Vertex v = interpolate(previousVertex, currentVertex);
+            //This is the vertex that you will find inside the screen not ouside
+            result_vertices.push_back(v);
+        }
+        */
+
+        if (currentInside) {
+            result_vertices.push_back(currentVertex);
+        }
+
+        previousVertex = currentVertex;
+        previousComponent = currentComponent;
+        previousInside = currentInside;
+    }
+    buildTriangles(result_vertices, finalListOfTriangles);
+};
+
+
+
+
+bool clipTest(Vertex a) {
+    double x = a.get_x();
+    double y = a.get_y();
+    double z = a.get_z();
+    return x <= 1 && x >= -1 && y <= 1 && y >= -1 && z <= 1 && z >= -1;
+}
+
+bool allGreater(Vertex a, double n) {
+    double x = a.get_x();
+    double y = a.get_y();
+    double z = a.get_z();
+    return (x > n && y > n && z > n);
+}
+
+std::list<Triangle> clipTriangle(Triangle &triangle) {
+    std::list<Triangle> triangles;
+    Vertex a, b, c;
+    a = triangle.get_a();
+    b = triangle.get_b();
+    c = triangle.get_c();
+
+}
+
+std::list<Triangle> clipTriangles(std::list<Triangle> &triangles) {
+    /* 3D Homogenous Clipping
+       In 1D with vertices {A, B, C}
+       A >= -1 && A <= 1
+       B >= -1 && B <= 1
+       C >= -1 && C <= 1 ? No.
+
+       So we take the segment between C and B and we find the point in this segment D
+       so that D >= -1 && D <= 1.
+
+       To find D we must interpolate between B and C.
+       Linear interpolation is
+       B(1 - L) + CL = 1 <==> B - LB + CL + (L - L) = 1 <==> ... <==>
+       L = (1 - B) / (1 - B) - (1 - C)
+    */
+    std::list<Triangle> result;
+
+    for (auto &triangle : triangles) {
+        /*
+            if (allOut(triangle.get_a(), 1) &&
+                    allOut(triangle.get_b(), 1) &&
+                    allOut(triangle.get_c(), 1)) {
+                continue;
+            }
+
+            if (allOut())
+            */
+    }
+
+    return result;
+}
--- Code/main.cpp
+++ Code/main.cpp
@@ -4,32 +4,54 @@
 #include <sstream>
 #include "Pipeline.h"
 
-bool loadObjFile(std::string filename, std::vector<Triangle> output)
+bool loadObjFile(const std::string& filename, std::vector<Triangle>& output)
 {
     std::ifstream file(filename);
     if (!file.is_open())
         return false;
-    std::stringstream ss;
     std::string line;
-    std::string vertex;
     while (!file.eof())
     {
         getline(std::cin, line);
-        //if(line[0] == "v")
-        /* while(std::getline(ss, vertex, 'v')){
+        if(line.rfind('v', 0) == 0){
+            std::stringstream ss(line);
+            std::string component;
             double x,y,z,w;
-            char c;
-            
-        }  */
+            int i;
+            std::vector<Vertex> vecV;
+            for(i = 0; getline(ss, component, ' '); ++i){
+                switch(i){
+                case 0:
+                    x = std::stod(component);
+                    break;
+                case 1:
+                    y = std::stod(component);
+                    break;
+                case 2:
+                    z = std::stod(component);
+                    break;
+                case 3:
+                    w = std::stod(component);
+                    break;
+                default:
+                    x = 1; y = 1; z= 1; w = 1;
+                    break;
+                }
+            }
+            if(i == 3)
+                w = 1;
+            Vertex v(x,y,z,w);
+            vecV.push_back(v);
+        }
     }
     return true;
 };
 
 int main()
 {
-    //float left, float right, float top, float bottom, float near, float far
+//float left, float right, float top, float bottom, float near, float far
     Projection p(-1, 1, -1, 1, 1, 2);
-    ScreenMapping sm(150, 50);
+    ScreenMapping sm(80, 80);
     CharFragmentShader shader;
     Clipping c;
 
